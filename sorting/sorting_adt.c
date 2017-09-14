ecfa9026ea7c27ae8c365bf77c89455ca4e0a7db
BhanuPrakashSingh <bhanu.singh932@gmail.com>
2017-09-14 10:23:18 +0530

    Adding merge sort ADT

--- sorting/sorting_adt.c
+++ sorting/sorting_adt.c
@@ -83,6 +83,76 @@ void optimise_bubble_sort(int arr[], int size)
 		}
 	}
 
+//--------------------------------------------------------------------------------------------------------------------------------
+//Merge sort
+/*Best Case	: O(nlogn)
+ *Average Case  : O(nlogn)
+ *Worst Case    : O(nlogn)
+ */
+void merge(int arr[],int start, int mid, int end)
+	{
+	int n1 = mid - start + 1;
+	int n2 = end - mid;
+	int Left_arr[n1];
+	int Right_arr[n2];
+	int i,j,k;
+
+	for(i=0;i<n1;i++)
+		{
+		Left_arr[i]=arr[start+i];	
+		}
+	for(j=0;j<n2;j++)
+		{
+		Right_arr[j]=arr[mid+1+j];
+		}
+
+	i=0;
+	j=0;
+	k=start;
+	while(i<n1 && j<n2)
+		{
+		if (Left_arr[i] <= Right_arr[j])
+			{
+			arr[k]=Left_arr[i];
+			i++;
+		} else {
+			arr[k]=Right_arr[j];
+			j++;
+		}
+		k++;
+		}
+	//copy remaining element from left array
+	while(i<n1)
+		{
+		arr[k] = Left_arr[i];
+		i++;
+		k++;
+		}
+	//copy remaining element from right array
+	while(j<n2)
+		{
+		arr[k] = Right_arr[j];
+		j++;
+		k++;
+		}
+	}
+
+void merge_sort(int arr[], int start, int end)
+	{
+	//
+	if(start < end)
+		{
+		int mid = start + (end-start)/2;
+		//sorting elements
+		merge_sort(arr,start,mid);
+		merge_sort(arr,mid+1,end);
+
+		//merge the sirted elements
+		merge(arr,start,mid,end);
+		}
+	}
+//--------------------------------------------------------------------------------------------------------------------------------
+
 //display
 void display(int arr[], int size)
 	{
@@ -98,11 +168,12 @@ void display(int arr[], int size)
 
 int main()
 	{
-	int arr[7]={5,3,6,2,8,21,1};
+	int arr[]={5,3,6,2,8,21,1};
 	int size=sizeof(arr)/sizeof(int);
 	//selection_sort(arr,size);
-	bubble_sort(arr,size);
+	//bubble_sort(arr,size);
 	//optimise_bubble_sort(arr,size);
+	merge_sort(arr,0,size-1);
 	display(arr,size);
 	return 0;
 	}
